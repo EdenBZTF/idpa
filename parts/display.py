ad0c530f839bde2bc05c7c1cd7d61a5d5e9a18e6
David Strainovic <108990357+davisko5@users.noreply.github.com>
2025-12-16 11:05:57 +0100

    change display

--- parts/display.py
+++ parts/display.py
@@ -1,26 +1,12 @@
-import board
-import busio
-from adafruit_ssd1306 import SSD1306_I2C
-from PIL import Image, ImageDraw, ImageFont
+from luma.core.interface.serial import i2c
+from luma.oled.device import ssd1306
+from luma.core.render import canvas
+from PIL import ImageFont
 
-# I2C initialisieren
-i2c = busio.I2C(board.SCL, board.SDA)
+# I2C-Interface initialisieren
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
 
-# OLED initialisieren (128x64)
-oled = SSD1306_I2C(128, 64, i2c)
-
-# Display löschen
-oled.fill(0)
-oled.show()
-
-# Bild erstellen
-image = Image.new("1", (oled.width, oled.height))
-draw = ImageDraw.Draw(image)
-
-# Text schreiben
-draw.text((0, 0), "Hello OLED!", fill=255)
-
-# Bild anzeigen
-oled.image(image)
-oled.show()
-``
+# Testanzeige
+with canvas(device) as draw:
+   
